2879020e8e5da2bac68f1a7d3dc76c0e9bf9a0ca
Aytan <aytangurbanova05@gmail.com>
2025-03-10 03:01:43 +0300

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:quizapp/quiz.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Quiz());
+}
---
+++ lib/quiz.dart
@@ -0,0 +1,76 @@
+import 'package:quizapp/data/questions.dart';
+import 'package:flutter/material.dart';
+import 'package:quizapp/start_screen.dart';
+import 'package:quizapp/questions_screen.dart';
+import 'package:quizapp/results_screen.dart';
+
+class Quiz extends StatefulWidget {
+  const Quiz({super.key});
+
+  @override
+  State<Quiz> createState() {
+    return _QuizState();
+  }
+}
+
+class _QuizState extends State<Quiz> {
+  List<String> selectedAnswers = [];
+  var activeScreen = 'Start-Screen';
+
+  void switchScreen() {
+    setState(() {
+      activeScreen = 'Questions-Screen';
+    });
+  }
+
+  void chooseAnswer(String answer) {
+    selectedAnswers.add(answer);
+
+    if (selectedAnswers.length == questions.length) {
+      setState(() {
+        activeScreen = 'Results-Screen';
+      });
+    }
+  }
+
+  void restartQuiz() {
+    setState(() {
+      selectedAnswers = [];
+      activeScreen = 'Questions-Screen';
+    });
+  }
+
+  @override
+  Widget build(context) {
+    Widget screenWidget = StartScreen(switchScreen);
+
+    if (activeScreen == 'Questions-Screen') {
+      screenWidget = QuestionsScreen(onSelectAnswer: chooseAnswer);
+    }
+
+    if (activeScreen == 'Results-Screen') {
+      screenWidget = ResultsScreen(
+        chosenAnswers: selectedAnswers,
+        onRestart: restartQuiz,
+      );
+    }
+
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                const Color.fromARGB(255, 144, 104, 254),
+                const Color.fromARGB(255, 9, 156, 224),
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: screenWidget,
+        ),
+      ),
+    );
+  }
+}
